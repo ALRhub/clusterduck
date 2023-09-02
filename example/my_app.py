b7ee7d4fdfd144f3d920d8c346b4db8d928ee7ee
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-09-02 12:06:41 +0200

    Update example app.

--- example/my_app.py
+++ example/my_app.py
@@ -1,11 +1,20 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import os
+import time
+
 import hydra
-from omegaconf import OmegaConf, DictConfig
+import numpy as np
+from omegaconf import DictConfig, OmegaConf
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def my_app(cfg: DictConfig) -> None:
+    print("Job config")
     print(OmegaConf.to_yaml(cfg))
+    print(f"CUDA_VISIBLE_DEVICES: {os.environ['CUDA_VISIBLE_DEVICES']}")
+    rng = np.random.default_rng()
+    duration = rng.integers(10)
+    print(f"Waiting {duration} seconds")
+    time.sleep(duration)
 
 
 if __name__ == "__main__":
