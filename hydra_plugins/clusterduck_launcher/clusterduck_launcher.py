b04db1d75414fdb416ed95c16e72593c029d36e7
Balazs Gyenes <balazs.gyenes@kit.edu>
2023-09-02 11:54:02 +0200

    Create a new process for each override.
    Refactor launcher to create a new process for each override.
    Resources are managed by manager process.

--- hydra_plugins/clusterduck_launcher/clusterduck_launcher.py
+++ hydra_plugins/clusterduck_launcher/clusterduck_launcher.py
@@ -1,12 +1,10 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import logging
+import multiprocessing as mp
 import os
+from multiprocessing.connection import wait
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Sequence
-from multiprocessing import Manager, Pool, current_process, Queue
-from joblib import Parallel, delayed  # type: ignore
 
-from hydra.core.hydra_config import HydraConfig
 from hydra.core.singleton import Singleton
 from hydra.core.utils import JobReturn, filter_overrides, run_job, setup_globals
 from hydra.plugins.launcher import Launcher
@@ -18,84 +16,15 @@ from .config import BaseQueueConf
 log = logging.getLogger(__name__)
 
 
-def run_run_job(
-        sweep_config: Dict,
-        alloc_queue: Queue,
-        hydra_context: HydraContext,
-        task_function: TaskFunction,
-        job_dir_key: str,
-) -> JobReturn:
-    import os
-
-    print(current_process().name)
-
-    sweep_config = OmegaConf.create(sweep_config)
-
-    gpu_idx = alloc_queue.get()
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_idx)
-
-    result = None
-    try:
-        result = run_job(
-            hydra_context=hydra_context,
-            task_function=task_function,
-            config=sweep_config,
-            job_dir_key=job_dir_key,
-            job_subdir_key="hydra.sweep.subdir",
-        )
-    except Exception as e:
-        log.error(f"Exception raised while executing job: {e}")
-    finally:
-        alloc_queue.put(gpu_idx)
-
-    return result
-
-def execute_job(
-    idx: int,
-    overrides: Sequence[str],
-    hydra_context: HydraContext,
-    config: DictConfig,
-    task_function: TaskFunction,
-    singleton_state: Dict[Any, Any],
-) -> JobReturn:
-    """Calls `run_job` in parallel"""
-    setup_globals()
-    Singleton.set_state(singleton_state)
-
-    sweep_config = hydra_context.config_loader.load_sweep_config(
-        config, list(overrides)
-    )
-    with open_dict(sweep_config):
-        sweep_config.hydra.job.id = f"{sweep_config.hydra.job.name}_{idx}"
-        sweep_config.hydra.job.num = idx
-    HydraConfig.instance().set_config(sweep_config)
-
-    ret = run_job(
-        hydra_context=hydra_context,
-        config=sweep_config,
-        task_function=task_function,
-        job_dir_key="hydra.sweep.dir",
-        job_subdir_key="hydra.sweep.subdir",
-    )
-
-    return ret
-
-def make_dictconfig_picklable(config: DictConfig, ConfigClass) -> DictConfig:
-    cfg = OmegaConf.merge(
-        OmegaConf.structured(DictConfig),
-        OmegaConf.create(OmegaConf.to_yaml(config, resolve=True))
-    )
-    return cfg
-
 class BaseClusterDuckLauncher(Launcher):
     _EXECUTOR = "abstract"
 
     def __init__(
-            self,
-            num_of_overrides_per_job: int = 1,
-            parallel_executions_in_job: int = 1,
-            exclusive_gpu_per_execution: bool = False,
-            **params: Any
+        self,
+        num_of_overrides_per_job: int = 1,
+        parallel_executions_in_job: int = 1,
+        exclusive_gpu_per_execution: bool = False,
+        **params: Any,
     ) -> None:
         self.params = {}
         for k, v in params.items():
@@ -112,7 +41,6 @@ class BaseClusterDuckLauncher(Launcher):
         self.parallel_executions_in_job = parallel_executions_in_job
         self.exclusive_gpu_per_execution = exclusive_gpu_per_execution
 
-
     def setup(
         self,
         *,
@@ -124,13 +52,70 @@ class BaseClusterDuckLauncher(Launcher):
         self.hydra_context = hydra_context
         self.task_function = task_function
 
-    def __call__(
+    def process_manager(
         self,
         sweep_overrides_list: List[List[str]],
         job_dir_key: str,
         job_num: int,
         job_id: str,
         singleton_state: Dict[type, Singleton],
+    ) -> list[JobReturn]:
+        processes: list[mp.Process] = []
+        n_processes = max(self.parallel_executions_in_job, len(sweep_overrides_list))
+        for i in range(n_processes):
+            # TODO: consider using forkserver context to create processes
+            process = mp.Process(
+                target=self,
+                kwargs=dict(
+                    sweep_overrides=sweep_overrides_list[i],
+                    job_dir_key=job_dir_key,
+                    job_num=job_num,
+                    job_id=job_id,
+                    singleton_state=singleton_state,
+                    resources=i,
+                ),
+            )
+            process.start()
+            processes.append(process)
+
+        submitted_overrides = n_processes
+        while submitted_overrides < len(sweep_overrides_list):
+            # wait for next override to finish
+            sentinels = [proc.sentinel for proc in processes]
+            done_processes = wait(sentinels)
+
+            for done_process in done_processes:
+                resource_id = sentinels.index(done_process)
+                processes[resource_id].join()
+                processes[resource_id] = mp.Process(
+                    target=self,
+                    kwargs=dict(
+                        sweep_overrides=sweep_overrides_list[submitted_overrides],
+                        job_dir_key=job_dir_key,
+                        job_num=job_num,
+                        job_id=job_id,
+                        singleton_state=singleton_state,
+                        resources=resource_id,
+                    ),
+                )
+                processes[resource_id].start()
+                submitted_overrides += 1
+                if submitted_overrides == len(sweep_overrides_list):
+                    break
+
+        for process in processes:
+            process.join()
+
+        # TODO: get return value from overrides and return here
+
+    def __call__(
+        self,
+        sweep_overrides: List[str],
+        job_dir_key: str,
+        job_num: int,
+        job_id: str,
+        singleton_state: Dict[type, Singleton],
+        resources,
     ) -> JobReturn:
         # lazy import to ensure plugin discovery remains fast
         import submitit
@@ -141,46 +126,26 @@ class BaseClusterDuckLauncher(Launcher):
 
         Singleton.set_state(singleton_state)
         setup_globals()
-        sweep_configs = []
-        for sweep_overrides in sweep_overrides_list:
-            sweep_config = self.hydra_context.config_loader.load_sweep_config(self.config, sweep_overrides)
-
-            with open_dict(sweep_config.hydra.job) as job:
-                # Populate new job variables
-                job.id = submitit.JobEnvironment().job_id  # type: ignore
-                sweep_config.hydra.job.num = job_num
-
-            sweep_configs.append(OmegaConf.to_container(sweep_config))
-
-
-        # m = Manager()
-        # alloc_queue = m.Queue()
-        # for i in range(self.parallel_executions_in_job):
-        #     alloc_queue.put(i)
-        # with Pool(self.parallel_executions_in_job) as pool:
-        #     a = pool.starmap(run_run_job, [(sweep_config, alloc_queue, self.hydra_context, self.task_function, job_dir_key) for sweep_config in sweep_configs])
-        # return a
-
-        runs = Parallel(
-            n_jobs=self.parallel_executions_in_job,
-            backend="loky",
-            prefer="processes",
-        )(
-        delayed(execute_job)(
-                job_num+idx,
-                overrides,
-                self.hydra_context,
-                sweep_config,
-                self.task_function,
-                singleton_state,
-            )
-            for idx, overrides in enumerate(sweep_configs)
+        sweep_config = self.hydra_context.config_loader.load_sweep_config(
+            self.config, sweep_overrides
         )
 
-        assert isinstance(runs, List)
-        for run in runs:
-            assert isinstance(run, JobReturn)
-        return runs
+        with open_dict(sweep_config.hydra.job) as job:
+            # Populate new job variables
+            job.id = submitit.JobEnvironment().job_id  # type: ignore
+            sweep_config.hydra.job.num = job_num
+
+        # assign resources
+        # TODO: abstract this
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(resources)
+
+        return run_job(
+            hydra_context=self.hydra_context,
+            task_function=self.task_function,
+            config=sweep_config,
+            job_dir_key=job_dir_key,
+            job_subdir_key="hydra.sweep.subdir",
+        )
 
     def checkpoint(self, *args: Any, **kwargs: Any) -> Any:
         """Resubmit the current callable at its current state with the same initial arguments."""
@@ -191,14 +156,15 @@ class BaseClusterDuckLauncher(Launcher):
 
     def launch(
         self, job_overrides: Sequence[Sequence[str]], initial_job_idx: int
-    ) -> Sequence[JobReturn]:
+    ) -> None:
         # lazy import to ensure plugin discovery remains fast
-        import submitit
         import math
+
+        import submitit
 
         assert self.config is not None
 
-        num_jobs = math.ceil(len(job_overrides)/self.num_of_overrides_per_job)
+        num_jobs = math.ceil(len(job_overrides) / self.num_of_overrides_per_job)
         assert num_jobs > 0
         params = self.params
         # build executor
@@ -225,7 +191,7 @@ class BaseClusterDuckLauncher(Launcher):
         executor.update_parameters(**params)
 
         log.info(
-            f"Submitit '{self._EXECUTOR}' sweep output dir : "
+            f"Clusterduck '{self._EXECUTOR}' sweep output dir : "
             f"{self.config.hydra.sweep.dir}"
         )
         sweep_dir = Path(str(self.config.hydra.sweep.dir))
@@ -236,24 +202,37 @@ class BaseClusterDuckLauncher(Launcher):
 
         job_params: List[Any] = []
         for idx in range(num_jobs):
-            job_overrides_sublist = job_overrides[idx*self.num_of_overrides_per_job:(idx+1)*self.num_of_overrides_per_job]
-            assert len(job_overrides_sublist) > 0 and len(job_overrides_sublist) <= self.num_of_overrides_per_job
+            job_overrides_sublist = job_overrides[
+                idx
+                * self.num_of_overrides_per_job : (idx + 1)
+                * self.num_of_overrides_per_job
+            ]
+            assert (
+                len(job_overrides_sublist) > 0
+                and len(job_overrides_sublist) <= self.num_of_overrides_per_job
+            )
+            idx = initial_job_idx + idx
 
-            filtered_job_overrides_sublist = [" ".join(filter_overrides(overrides)) for overrides in job_overrides_sublist]
-            log.info(f"Launching {filtered_job_overrides_sublist} in job with id {initial_job_idx+idx}")
+            filtered_job_overrides_sublist = [
+                " ".join(filter_overrides(overrides))
+                for overrides in job_overrides_sublist
+            ]
+            log.info(f"Launching {filtered_job_overrides_sublist} in job with id {idx}")
 
             job_params.append(
                 (
                     job_overrides_sublist,
                     "hydra.sweep.dir",
-                    idx+initial_job_idx,
-                    f"job_id_for_{idx+initial_job_idx}",
+                    idx,  # TODO: adjust so that each override has unique idx?
+                    f"job_id_for_{idx}",
                     Singleton.get_state(),
                 )
             )
 
-        jobs = executor.map_array(self, *zip(*job_params))
-        return [j.results()[0] for j in jobs]
+        # launch all
+        executor.map_array(self.process_manager, *zip(*job_params))
+
+        # TODO: should there be any return value here? we don't want to wait for jobs to complete
 
 
 class ClusterDuckLocalLauncher(BaseClusterDuckLauncher):
